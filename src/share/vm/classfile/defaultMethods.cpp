146b4fca8b8a32f2ec16ffc125a7ca4a95c90545
Yichen Yan <wenji.yyc@alibaba-inc.com>
2021-07-16 10:03:57 +0800

    8219713

--- src/share/vm/classfile/defaultMethods.cpp
+++ src/share/vm/classfile/defaultMethods.cpp
@@ -153,7 +153,7 @@ class HierarchyVisitor : StackObj {
     }
   };
 
-  bool _cancelled;
+  bool _visited_Object;
   GrowableArray<Node*> _path;
 
   Node* current_top() const { return _path.top(); }
@@ -161,22 +161,19 @@ class HierarchyVisitor : StackObj {
   void push(InstanceKlass* cls, void* data) {
     assert(cls != NULL, "Requires a valid instance class");
     Node* node = new Node(cls, data, has_super(cls));
+    if (cls == SystemDictionary::Object_klass()) {
+      _visited_Object = true;
+    }
     _path.push(node);
   }
   void pop() { _path.pop(); }
 
-  void reset_iteration() {
-    _cancelled = false;
-    _path.clear();
-  }
-  bool is_cancelled() const { return _cancelled; }
-
-  // This code used to skip interface classes because their only
-  // superclass was j.l.Object which would be also covered by class
-  // superclass hierarchy walks. Now that the starting point can be
-  // an interface, we must ensure we catch j.l.Object as the super.
-  static bool has_super(InstanceKlass* cls) {
-    return cls->super() != NULL;
+  // Since the starting point can be an interface, we must ensure we catch
+  // j.l.Object as the super once in those cases. The _visited_Object flag
+  // only ensures we don't then repeatedly enqueue Object for each interface
+  // in the class hierarchy.
+  bool has_super(InstanceKlass* cls) {
+    return cls->super() != NULL && (!_visited_Object || !cls->is_interface());
   }
 
   Node* node_at_depth(int i) const {
@@ -200,14 +197,10 @@ class HierarchyVisitor : StackObj {
   }
   void* current_data() { return data_at_depth(0); }
 
-  void cancel_iteration() { _cancelled = true; }
-
  public:
 
   void run(InstanceKlass* root) {
     ALGO* algo = static_cast<ALGO*>(this);
-
-    reset_iteration();
 
     void* algo_data = algo->new_node_data(root);
     push(root, algo_data);
@@ -242,7 +235,7 @@ class HierarchyVisitor : StackObj {
         push(next, algo_data);
         top_needs_visit = true;
       }
-    } while (!is_cancelled() && has_more_nodes());
+    } while (has_more_nodes());
   }
 };
 
@@ -540,22 +533,16 @@ class StatefulMethodFamily : public ResourceObj {
   }
 
  protected:
-  MethodFamily* _method_family;
+  MethodFamily _method_family;
 
  public:
   StatefulMethodFamily() {
-   _method_family = new MethodFamily();
    _qualification_state = QUALIFIED;
   }
 
-  StatefulMethodFamily(MethodFamily* mf) {
-   _method_family = mf;
-   _qualification_state = QUALIFIED;
-  }
+  void set_target_if_empty(Method* m) { _method_family.set_target_if_empty(m); }
 
-  void set_target_if_empty(Method* m) { _method_family->set_target_if_empty(m); }
-
-  MethodFamily* get_method_family() { return _method_family; }
+  MethodFamily* get_method_family() { return &_method_family; }
 
   StateRestorer* record_method_and_dq_further(Method* mo);
 };
@@ -575,9 +562,9 @@ class StateRestorer : public PseudoScopeMark {
 StateRestorer* StatefulMethodFamily::record_method_and_dq_further(Method* mo) {
   StateRestorer* mark = new StateRestorer(this, _qualification_state);
   if (_qualification_state == QUALIFIED) {
-    _method_family->record_qualified_method(mo);
+    _method_family.record_qualified_method(mo);
   } else {
-    _method_family->record_disqualified_method(mo);
+    _method_family.record_disqualified_method(mo);
   }
   // Everything found "above"??? this method in the hierarchy walk is set to
   // disqualified
@@ -627,12 +614,10 @@ static bool already_in_vtable_slots(GrowableArray<EmptyVtableSlot*>* slots, Meth
   return found;
 }
 
-static GrowableArray<EmptyVtableSlot*>* find_empty_vtable_slots(
+static void find_empty_vtable_slots(GrowableArray<EmptyVtableSlot*>* slots,
     InstanceKlass* klass, GrowableArray<Method*>* mirandas, TRAPS) {
 
   assert(klass != NULL, "Must be valid class");
-
-  GrowableArray<EmptyVtableSlot*>* slots = new GrowableArray<EmptyVtableSlot*>();
 
   // All miranda methods are obvious candidates
   for (int i = 0; i < mirandas->length(); ++i) {
@@ -653,9 +638,9 @@ static GrowableArray<EmptyVtableSlot*>* find_empty_vtable_slots(
         // default method processing that occurred on behalf of our superclass,
         // so it's a method we want to re-examine in this new context.  That is,
         // unless we have a real implementation of it in the current class.
-        Method* impl = klass->lookup_method(m->name(), m->signature());
-        if (impl == NULL || impl->is_overpass() || impl->is_static()) {
-          if (!already_in_vtable_slots(slots, m)) {
+        if (!already_in_vtable_slots(slots, m)) {
+          Method *impl = klass->lookup_method(m->name(), m->signature());
+          if (impl == NULL || impl->is_overpass() || impl->is_static()) {
             slots->append(new EmptyVtableSlot(m));
           }
         }
@@ -670,9 +655,9 @@ static GrowableArray<EmptyVtableSlot*>* find_empty_vtable_slots(
         // default method processing that occurred on behalf of our superclass,
         // so it's a method we want to re-examine in this new context.  That is,
         // unless we have a real implementation of it in the current class.
-        Method* impl = klass->lookup_method(m->name(), m->signature());
-        if (impl == NULL || impl->is_overpass() || impl->is_static()) {
-          if (!already_in_vtable_slots(slots, m)) {
+        if (!already_in_vtable_slots(slots, m)) {
+          Method* impl = klass->lookup_method(m->name(), m->signature());
+          if (impl == NULL || impl->is_overpass() || impl->is_static()) {
             slots->append(new EmptyVtableSlot(m));
           }
         }
@@ -692,7 +677,6 @@ static GrowableArray<EmptyVtableSlot*>* find_empty_vtable_slots(
     }
   }
 #endif // ndef PRODUCT
-  return slots;
 }
 
 // Iterates over the superinterface type hierarchy looking for all methods
@@ -764,8 +748,7 @@ static void create_defaults_and_exceptions(
     GrowableArray<EmptyVtableSlot*>* slots, InstanceKlass* klass, TRAPS);
 
 static void generate_erased_defaults(
-     InstanceKlass* klass, GrowableArray<EmptyVtableSlot*>* empty_slots,
-     EmptyVtableSlot* slot, TRAPS) {
+     InstanceKlass* klass, EmptyVtableSlot* slot, TRAPS) {
 
   // sets up a set of methods with the same exact erased signature
   FindMethodsByErasedSig visitor(slot->name(), slot->signature());
@@ -823,11 +806,11 @@ void DefaultMethods::generate_default_methods(
   }
 #endif // ndef PRODUCT
 
-  GrowableArray<EmptyVtableSlot*>* empty_slots =
-      find_empty_vtable_slots(klass, mirandas, CHECK);
+  GrowableArray<EmptyVtableSlot*> empty_slots;
+  find_empty_vtable_slots(&empty_slots, klass, mirandas, CHECK);
 
-  for (int i = 0; i < empty_slots->length(); ++i) {
-    EmptyVtableSlot* slot = empty_slots->at(i);
+  for (int i = 0; i < empty_slots.length(); ++i) {
+    EmptyVtableSlot* slot = empty_slots.at(i);
 #ifndef PRODUCT
     if (TraceDefaultMethods) {
       streamIndentor si(tty, 2);
@@ -837,7 +820,7 @@ void DefaultMethods::generate_default_methods(
     }
 #endif // ndef PRODUCT
 
-    generate_erased_defaults(klass, empty_slots, slot, CHECK);
+    generate_erased_defaults(klass, slot, CHECK);
  }
 #ifndef PRODUCT
   if (TraceDefaultMethods) {
@@ -845,7 +828,7 @@ void DefaultMethods::generate_default_methods(
   }
 #endif // ndef PRODUCT
 
-  create_defaults_and_exceptions(empty_slots, klass, CHECK);
+  create_defaults_and_exceptions(&empty_slots, klass, CHECK);
 
 #ifndef PRODUCT
   if (TraceDefaultMethods) {
@@ -930,8 +913,7 @@ static void switchover_constant_pool(BytecodeConstantPool* bpool,
 // This allows virtual methods to override the overpass, but ensures
 // that a local method search will find the exception rather than an abstract
 // or default method that is not a valid candidate.
-static void create_defaults_and_exceptions(
-    GrowableArray<EmptyVtableSlot*>* slots,
+static void create_defaults_and_exceptions(GrowableArray<EmptyVtableSlot*>* slots,
     InstanceKlass* klass, TRAPS) {
 
   GrowableArray<Method*> overpasses;
@@ -996,7 +978,7 @@ static void create_defaults_and_exceptions(
   }
 }
 
-static void create_default_methods( InstanceKlass* klass,
+static void create_default_methods(InstanceKlass* klass,
     GrowableArray<Method*>* new_methods, TRAPS) {
 
   int new_size = new_methods->length();
